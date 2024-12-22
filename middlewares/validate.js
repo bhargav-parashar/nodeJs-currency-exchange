37460ea6c77011d1338ba31fd71f28a445edf1fb
Bhargav Parashar <bhargavparashar99@gmail.com>
2024-12-23 00:06:14 +0530

    Created function which returns generalized Middleware to validate query params

--- middlewares/validate.js
+++ middlewares/validate.js
@@ -1,11 +1,18 @@
-const {convertSchema} = require("../validations/exchange.validtions.js");
+// const {convertSchema} = require("../validations/exchange.validtions.js");
 
-const exchangeValidator = (req, res, next) =>{
+// const exchangeValidator = (req, res, next) =>{
 
-    const{value, currency, to_currency} = req.query;
-    const {error} = convertSchema.validate({value,currency,to_currency});
+//     const{value, currency, to_currency} = req.query;
+//     const {error} = convertSchema.validate({value,currency,to_currency});
 
+//     if(error) return  res.status(400).send({message : "Incomplete or Incorrect data passed"});
+//     next();
+// }
+
+const queryValidator =(schema)=>(req, res, next) =>{
+    const {error} = schema.validate(req.query);
     if(error) return  res.status(400).send({message : "Incomplete or Incorrect data passed"});
     next();
 }
-module.exports = {exchangeValidator};
+
+module.exports = {queryValidator};
--- routes/exchange.routes.js
+++ routes/exchange.routes.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const { getCurrencies,convertCurrencies } = require("../controllers/exchange.controllers.js");
-const {exchangeValidator} = require("../middlewares/validate.js"); //validator middleware
+const {convertSchema} = require("../validations/exchange.validtions.js");
+
+//const {exchangeValidator} = require("../middlewares/validate.js"); validator middleware
+
+const {queryValidator} = require("../middlewares/validate.js"); // importing general query validator function which returns a middleware
+const exchangeValidator = queryValidator(convertSchema) // Calling the general function which will return a middleware instance
 
 router.get("/currencies",getCurrencies);
-router.get("/convert",exchangeValidator, convertCurrencies) // path, middleware, controller
+
+//router.get("/convert",exchangeValidator, convertCurrencies) path, middleware, controller
+
+router.get("/convert",exchangeValidator,convertCurrencies); //path, middleware, controller
 
 module.exports = router;
